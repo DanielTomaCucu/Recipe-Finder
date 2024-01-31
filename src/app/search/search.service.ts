505b51dcbb79748b616d9981d0b36265554de2a4
Cucu Toma Daniel <cucutomadaniel@gmail.com>
2024-01-31 13:02:29 +0200

    add list of items

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,6 +8,8 @@ import { ApiService } from '../api.service';
 export class SearchService {
   constructor(private apiService: ApiService) {}
 
+  private recipeSubject = new BehaviorSubject<any>(null);
+  recipes$ = this.recipeSubject.asObservable();
   searchRecipes(searchQuery: string, selectedCuisines: string[]) {
     const params = {
       query: searchQuery,
@@ -14,8 +17,8 @@ export class SearchService {
       addRecipeInformation: true,
       number: 15,
     };
-    this.apiService
-      .getApiData('complexSearch', params)
-      .subscribe((data) => console.log(data));
+    this.apiService.getApiData('complexSearch', params).subscribe((data) => {
+      console.log(data), this.recipeSubject.next(data);
+    });
   }
 }
---
+++ src/app/shared/dummydata.ts
@@ -0,0 +1,51 @@
+export const dummyData = [
+  {
+    title: 'Duck Rumaki',
+    image: '../../../assets/icons/dummyImg.png',
+    pricePerServing: 29.44,
+    readyInMinutes: 45,
+    servings: 24,
+  },
+  {
+    title: 'Garlic Parmesan Chicken',
+    image: 'https://spoonacular.com/recipeImages/715497-312x231.jpg',
+    pricePerServing: 34.56,
+    readyInMinutes: 50,
+    servings: 4,
+  },
+  {
+    title: 'Classic Margherita Pizza',
+    image: 'https://spoonacular.com/recipeImages/715415-312x231.jpg',
+    pricePerServing: 18.34,
+    readyInMinutes: 30,
+    servings: 2,
+  },
+  {
+    title: 'Vegan Chocolate Cake',
+    image: 'https://spoonacular.com/recipeImages/716426-312x231.jpg',
+    pricePerServing: 50.23,
+    readyInMinutes: 75,
+    servings: 8,
+  },
+  {
+    title: 'Spicy Thai Noodles',
+    image: 'https://spoonacular.com/recipeImages/716627-312x231.jpg',
+    pricePerServing: 15.99,
+    readyInMinutes: 25,
+    servings: 3,
+  },
+  {
+    title: 'Quinoa Salad with Vegetables',
+    image: 'https://spoonacular.com/recipeImages/782601-312x231.jpg',
+    pricePerServing: 12.45,
+    readyInMinutes: 20,
+    servings: 2,
+  },
+  {
+    title: 'Beef Bourguignon',
+    image: 'https://spoonacular.com/recipeImages/794349-312x231.jpg',
+    pricePerServing: 45.88,
+    readyInMinutes: 120,
+    servings: 6,
+  },
+];
