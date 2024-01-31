5206e9c8ea87f017b8cdd478cf1ea804fd973b41
Cucu Toma Daniel <cucutomadaniel@gmail.com>
2024-01-31 16:18:55 +0200

    add more data

---
+++ src/app/interfaces/apiResponse.ts
@@ -0,0 +1,6 @@
+export interface ApiResponse {
+  results: any[];
+  totalResults: number;
+  number: number;
+  offset: number;
+}
--- src/app/search/searched-cuisines/searched-cuisines.component.ts
+++ src/app/search/searched-cuisines/searched-cuisines.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
 import { dummyData } from 'src/app/shared/dummydata';
+import { Subscription } from 'rxjs';
+import { InfiniteScrollCustomEvent,  } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-searched-cuisines',
   templateUrl: './searched-cuisines.component.html',
-  styleUrls: [
-    './searched-cuisines.component.scss',
-
-  ],
+  styleUrls: ['./searched-cuisines.component.scss'],
 })
-export class SearchedCuisinesComponent implements OnInit {
-  constructor(private searchService: SearchService) {}
-  recipes: any='';
+export class SearchedCuisinesComponent implements OnInit, OnDestroy {
+  constructor(
+    private searchService: SearchService,
+
+  ) {}
+  private subs: Subscription = new Subscription();
+  recipes: any = [];
+  loading: boolean = false;
   ngOnInit() {
+     console.log(this.recipes);
+    this.subs.add(
+      this.searchService.loading$.subscribe(
+        (loading) => (this.loading = loading)
+      )
+    );
+    this.subs.add(
       this.searchService.recipes$.subscribe((data) => {
-      console.log(data);
-      this.recipes = data;
-    });
-    /* this.recipes = dummyData;
-    console.log(this.recipes); */
+
+        this.recipes = data;
+         console.log(this.recipes);
+      })
+    );
+  }
+
+  onIonInfinite(ev: InfiniteScrollCustomEvent) {
+    this.fetchMoreData();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 1000);
+  }
+
+  fetchMoreData() {
+    this.searchService.searchRecipes(
+      this.searchService.searchQuery,
+      this.searchService.selectedCuisines
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+    this.searchService.resetData();
   }
 }
